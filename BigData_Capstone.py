1d4edf1701a6de918fb013fa2b7041d5a986cddb
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2022-05-16 16:06:39 +0200

    Update BigData_Capstone.py

--- BigData_Capstone.py
+++ BigData_Capstone.py
@@ -6,17 +6,6 @@ Created on Tue May 10 15:39:15 2022
 """
 
 ################################################
-# Setup
-################################################
-
-#Pfad jeweils an die eigene Ablage anpassen
-path = "G:/Meine Ablage/Colab Notebooks/Capstone/Programm/" #Pfadi Fabio
-
-# Um das Programm zu starten:
-# 1. Anaconda Prompt starten, 2. Programm ausführen, 3. Konsolen-Ouput in Prompot kopieren und ausführen
-print('streamlit run ' + '"' + path + 'BigData_Capstone.py"')
-
-################################################
 # Bibliotheken importieren & Daten Laden
 ################################################
 
@@ -36,12 +25,12 @@ st.set_page_config(
 # Daten importieren
 # @st.cache(allow_output_mutation=True)
 def load_data():
-    df = pd.read_csv(path + "houses_data_dev.csv")
+    df = pd.read_csv("houses_data_dev.csv")
     return df
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    filename = path + 'final_model.sav'
+    filename = 'final_model.sav'
     loaded_model = pickle.load(open(filename, "rb"))
     return(loaded_model)
 
