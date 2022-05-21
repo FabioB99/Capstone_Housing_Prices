a60789250cd058e351e4b163621e9d3e19e55150
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2022-05-21 21:27:22 +0200

    Update BigData_Capstone.py

--- BigData_Capstone.py
+++ BigData_Capstone.py
@@ -116,7 +116,6 @@ einfahrt = st.sidebar.slider("Länge der Einfahrt in ft",
 ################################################
 
 st.subheader("Visualisierung der Daten:")
-
 st.write("#")
 
 col1, col2, col3  = st. columns([1,1,1])
@@ -125,17 +124,20 @@ baujahr2 = col1.slider("Baujahr", 1850, 2022, (2000, 2010))
 quality2 = col2.slider('Zustand der Immobilie', 0, 10, (0,10))
 grundstücksfläche2 = col3.slider('Grundstücksfläche in sqf', 0, 250000, (150000, 50000))
 badezimmer2 = col1.number_input("Anzahl Badezimmer", 0, 200, 3)
+garagenplätze = col1.number_input("Anzahl Garagenplätze", 0, 200, 2)
 
 # creating filtered data set according to slider inputs
 filtered_df = df.loc[(df["OverallQual"] >= quality2[0]) & (df["OverallQual"] <= quality2[1]) &
                      (df["LotArea"] >= grundstücksfläche2[0]) & (df["LotArea"] <= grundstücksfläche2[1]) &
                      (df["YearBuilt"] >= baujahr2[0]) & (df["YearBuilt"] <= baujahr2[1]) &
-                     (df["FullBath"] >= badezimmer2)
+                     (df["FullBath"] >= badezimmer2) & (df["GarageCars"] >= garagenplätze)
+                     
                      ,:]
 
 with st.expander("Ganzes Datenset anzeigen"):
     st.dataframe(filtered_df)
 
+st.write("#")
 
 ################################################
 # Vorhersage
