caef31b971cb51b883b90f5f0d8c0a3f438dd235
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2022-05-21 18:17:22 +0200

    Update BigData_Capstone.py

--- BigData_Capstone.py
+++ BigData_Capstone.py
@@ -148,13 +148,7 @@ if st.button("Start Prediction"):
 with st.expander("Vorhersage anhand Datenset durchühren"):
     uploaded_data = st.file_uploader("Datenset auswählen:") 
     
-st.markdown(""" div.stButton > button:first-child {
-background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
-}
-(""", unsafe_allow_html=True)
 
-if st.button("the notice you want to show"):
-    st.write("content you want to show")
 
 st.write("#")
 
