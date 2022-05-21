9d807ce28e149be54bf3ede72273e00fd7e3b462
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2022-05-21 17:48:16 +0200

    Update BigData_Capstone.py

--- BigData_Capstone.py
+++ BigData_Capstone.py
@@ -140,12 +140,12 @@ def make_prediction():
     st.write("#")    
     st.metric(label="Estimated Price:", value=price_output)
 
-row1_col1, row1_col2, row1_col3 = st.columns([1,1,1])
+row1_col1, row1_col2 = st.columns([1,1])
 
 if row1_col2.button("Start Prediction"):
     st.write(make_prediction())
     
-uploaded_data = row1_col1.file_uploader("Choose a file with Customer Data for Predicting Customer Default")
+uploaded_data = st.file_uploader("Choose a file with Customer Data for Predicting Customer Default")
 
 
 
